d4d72626c31e5f18ab29f62d74e6bb6b2fadd266
Dave <100411475+CoderKuno@users.noreply.github.com>
2023-04-27 23:51:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hiram/Scan.java
@@ -0,0 +1,16 @@
+package com.example.hiram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Scan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+    }
+}
